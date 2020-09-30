8264d57159e69df1087207f118b716bd2b45af7e
Rhuan Yago <rhuan.yago2014@live.com>
2020-09-30 12:00:29 -0300

    teste

--- teste.php
+++ teste.php
@@ -1,13 +1,20 @@
 <?php
 
-    $segundos = '85983';
+    $segundos = '1846';
     $dtF = new \DateTime('@0');
     $dtT = new \DateTime("@$segundos");
-   
+
     $dateDiff = $dtF->diff($dtT);
 
+    if ($dateDiff->d > 0) {
+        $tempoConexaoDisp = date('d/m/Y H:i:s', strtotime('-'.$dateDiff->d.' day'));
+    }elseif ($dateDiff->h > 0) {
+        $tempoConexaoDisp = date('d/m/Y H:i:s', strtotime('-'.$dateDiff->h.' hours'));
+    }elseif ($dateDiff->i > 0) {
+        $tempoConexaoDisp = date('d/m/Y H:i:s', strtotime('-'.$dateDiff->i.' minutes'));
+    }
 
-    echo $data = date('d/m/Y H:i:s', strtotime('-'.$dateDiff->d.' day'));
+    echo $tempoConexaoDisp;
 
    
 
