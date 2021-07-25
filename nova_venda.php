2dc688ef1b7b9f122bce8b4b745a84dae525ccdc
rhuanyago <rhuan.yago2014@live.com>
2021-07-25 13:56:55 -0300

    ajuste estoque

--- nova_venda.php
+++ nova_venda.php
@@ -128,7 +128,7 @@ $rowListar = $obj->listarItens($idpedido);
                             <div class="col-sm-2">
                                 <div class="form-group">
                                     <label class=" control-label text-weight-bold">Referência</label>
-                                    <input type="text" name="referencia" id="referencia" class="form-control text-weight-bold" maxlength="8" upper >
+                                    <input type="text" name="referencia" id="referencia" class="form-control text-weight-bold" maxlength="8" upper>
                                 </div>
                             </div>
                             <div class="col-sm-6">
@@ -179,7 +179,7 @@ $rowListar = $obj->listarItens($idpedido);
                                         </div>
                                         <input type="text" readonly value="0" name="qtde" id="qtde" data-estoque="" class="form-control spinnerVal spinner-roundVal" />
                                         <div class="input-group-append">
-                                            <button type="button" class="btn btn-primary text-light spinnerbutton spinnerPlus spinner-roundbutton">
+                                            <button type="button" id="spinnerPlus" class="btn btn-primary text-light spinnerbutton spinnerPlus spinner-roundbutton">
                                                 <span class="fa fa-plus"></span>
                                             </button>
                                         </div>
@@ -369,6 +369,7 @@ $rowListar = $obj->listarItens($idpedido);
 <script>
     $(document).ready(function() {
 
+
         /* alert("ready");//Thank You Saviour */
         var minusButton = $(".spinnerMinus"); //to aquire all minus buttons
         var plusButton = $(".spinnerPlus"); //to aquire all plus buttons
@@ -377,16 +378,16 @@ $rowListar = $obj->listarItens($idpedido);
             var estoque = $('#qtde').attr('data-estoque');
 
             trigger_Spinner($(this), "-", {
-                max: 1000,
+                max: estoque,
                 min: 1,
             }); //Triggers the Spinner Actuator
         }); /*end Handle Minus Button click*/
 
         plusButton.click(function() {
-            // var estoque = $('#qtde').attr('data-estoque');
+            var estoque = $('#qtde').attr('data-estoque');
 
             trigger_Spinner($(this), "+", {
-                max: 1000,
+                max: estoque,
                 min: 1,
             }); //Triggers the Spinner Actuator    
         }); /*end Handle Plus Button Click*/
@@ -409,6 +410,7 @@ $rowListar = $obj->listarItens($idpedido);
                     url: "classes/conect.php",
                     dataType: "json",
                     success: function(result) {
+                        $("#spinnerPlus").prop("disabled", false);
                         if (typeof result !== 'undefined') {
                             var referencia = result["referencia"];
                             var preco = result["preco"];
@@ -450,6 +452,8 @@ $rowListar = $obj->listarItens($idpedido);
 
             var referencia = $('#referencia').val();
 
+            $("#spinnerPlus").prop("disabled", false);
+
             if (referencia.length <= 1 || referencia == '') {
                 // $("#referencia").val('');
                 $("#preco").val('');
@@ -462,6 +466,8 @@ $rowListar = $obj->listarItens($idpedido);
                 var element = document.getElementById("addProduto");
                 element.classList.remove("oculto");
                 var estoqueIndis = document.getElementById("estoqueIndis");
+                estoqueIndis.classList.add("oculto");
+                var estoqueIndis = document.getElementById("estoque1");
                 estoqueIndis.classList.add("oculto");
             }
 
@@ -607,6 +613,8 @@ $rowListar = $obj->listarItens($idpedido);
         //dados=$('#frmCategoriaU').serialize();    
 
         if (referencia == "") {
+            alertify.set('notifier', 'position', 'top-center');
+            alertify.error('Campo Referência em Branco!');
             return;
         }
 
@@ -754,195 +762,5 @@ $rowListar = $obj->listarItens($idpedido);
 
     });
 </script>
-
-<!-- <script>
-
-var cont = 0;
-var tabela = 0;
-gerarDadosUP();
-function gerarDadosUP(){
-    var idpedido = document.getElementById("idpedido").value;
-    $.ajax({
-        type: 'POST',
-        url: 'classes/conect.php',
-        data: {
-            idpedido:idpedido,
-            paramTela: 'listarItens'    
-        },
-        success: function(data){  
-            //$('#loading_table2').hide();                                                                                                          
-            dados = null;
-            dados = JSON.parse(data);
-                if(cont == 0){
-                    cont++
-                }else {
-                    tabela.destroy();
-                }                    
-            // dados = [dados];
-            tabela = $('#listarItens').DataTable({  
-                    "aaSorting": [[1, "asc"]],  //ordenação da 2° coluna do datatable para descendente 
-                    "bInfo" :false,         
-                    "scrollX": true, // cria o scroll no dataTable   
-                    paging: false,     
-                    searching: false,   
-                    ordering: false,                                                          
-    				data: dados, 
-                    columns: [ //head da table direto no javascript
-                        {title: 'Referencia' },    
-                        {title: 'Descricao' },                        
-                        {title: 'Qtde' },    
-                        {title: 'Preco Unit.' }, 
-                        {title: 'Preco Bruto' },     
-                        {title: 'Opcoes'  },                                               
-                    ],
-                    "columnDefs": [
-                        { "width": "10%", "targets": 2 }
-                    ],
-                    language: { //PROPRIEDADES DO DATATABLE
-						"sEmptyTable": "Nenhum registro encontrado nesse período",
-						"sInfo": "Mostrando de _START_ até _END_ de _TOTAL_ registros",
-						"sInfoEmpty": "Mostrando 0 até 0 de 0 registros",
-						"sInfoFiltered": "(Filtrados de MAX registros)",
-						"sInfoPostFix": "",
-						"sInfoThousands": ".",
-						"sLengthMenu": "_MENU_ Resultados por página",
-						"sLoadingRecords": "Carregando...",
-						"sProcessing": "Processando...",
-						"sZeroRecords": "Nenhum registro encontrado",
-						"sSearch": "Pesquisar",
-						"oPaginate": {
-							"sNext": "Próximo",
-							"sPrevious": "Anterior",
-							"sFirst": "Primeiro",
-							"sLast": "Último"
-						},
-						"oAria": {
-							"sSortAscending": ": Ordenar colunas de forma ascendente",
-							"sSortDescending": ": Ordenar colunas de forma descendente"
-						},
-						"buttons": {
-								"copyTitle": "Copiar linhas"
-							}
-						},
-                        // "columnDefs": [
-                        //     {
-                        //         "targets": [ 0 ],
-                        //         "visible": false,
-                        //         "searchable": false
-                        //     },
-                        // ] 				
-                }).row.add([
-                     '', '<td class="text-right" style="color:red;"><b>Total<b></td>', '<td class="text-center"><b>', '<td class="text-center"><b><b></td>', '', ''
-                    ]).draw();
-            }				
-        })
-    } 
-</script> -->
-<!-- 
-<script>
-    function autocomplete(inp, arr) {
-        /*the autocomplete function takes two arguments,
-        the text field element and an array of possible autocompleted values:*/
-        var currentFocus;
-        /*execute a function when someone writes in the text field:*/
-        inp.addEventListener("input", function(e) {
-            var a, b, i, val = this.value;
-            closeAllLists();
-            if (!val) {
-                return false;
-            }
-            currentFocus = -1;
-            a = document.createElement("DIV");
-            a.setAttribute("id", this.id + "autocomplete-list");
-            a.setAttribute("class", "autocomplete-items");
-            /*append the DIV element as a child of the autocomplete container:*/
-            this.parentNode.appendChild(a);
-            /*for each item in the array...*/
-            for (i = 0; i < arr.length; i++) {
-                /*check if the item starts with the same letters as the text field value:*/
-                if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
-                    /*create a DIV element for each matching element:*/
-                    b = document.createElement("DIV");
-                    /*make the matching letters bold:*/
-                    b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
-                    b.innerHTML += arr[i].substr(val.length);
-                    /*insert a input field that will hold the current array item's value:*/
-                    b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
-                    /*execute a function when someone clicks on the item value (DIV element):*/
-                    b.addEventListener("click", function(e) {
-                        /*insert the value for the autocomplete text field:*/
-                        inp.value = this.getElementsByTagName("input")[0].value;
-                        closeAllLists();
-                    });
-                    a.appendChild(b);
-                }
-            }
-        });
-        /*execute a function presses a key on the keyboard:*/
-        inp.addEventListener("keydown", function(e) {
-            var x = document.getElementById(this.id + "autocomplete-list");
-            if (x) x = x.getElementsByTagName("div");
-            if (e.keyCode == 40) {
-                /*If the arrow DOWN key is pressed,
-                increase the currentFocus variable:*/
-                currentFocus++;
-                /*and and make the current item more visible:*/
-                addActive(x);
-            } else if (e.keyCode == 38) { //up
-                /*If the arrow UP key is pressed,
-                decrease the currentFocus variable:*/
-                currentFocus--;
-                /*and and make the current item more visible:*/
-                addActive(x);
-            } else if (e.keyCode == 13) {
-                /*If the ENTER key is pressed, prevent the form from being submitted,*/
-                e.preventDefault();
-                if (currentFocus > -1) {
-                    /*and simulate a click on the "active" item:*/
-                    if (x) x[currentFocus].click();
-                }
-            }
-        });
-
-        function addActive(x) {
-            /*a function to classify an item as "active":*/
-            if (!x) return false;
-            /*start by removing the "active" class on all items:*/
-            removeActive(x);
-            if (currentFocus >= x.length) currentFocus = 0;
-            if (currentFocus < 0) currentFocus = (x.length - 1);
-            /*add class "autocomplete-active":*/
-            x[currentFocus].classList.add("autocomplete-active");
-        }
-
-        function removeActive(x) {
-            /*a function to remove the "active" class from all autocomplete items:*/
-            for (var i = 0; i < x.length; i++) {
-                x[i].classList.remove("autocomplete-active");
-            }
-        }
-
-        function closeAllLists(elmnt) {
-            /*close all autocomplete lists in the document,
-            except the one passed as an argument:*/
-            var x = document.getElementsByClassName("autocomplete-items");
-            for (var i = 0; i < x.length; i++) {
-                if (elmnt != x[i] && elmnt != inp) {
-                    x[i].parentNode.removeChild(x[i]);
-                }
-            }
-        }
-        /*execute a function when someone clicks in the document:*/
-        document.addEventListener("click", function(e) {
-            closeAllLists(e.target);
-        });
-    }
-
-    /*An array containing all the country names in the world:*/
-    var countries = ["Afghanistan", "Albania", "Algeria", "Andorra", "Angola", "Anguilla", "Antigua & Barbuda", "Argentina", "Armenia", "Aruba", "Australia", "Austria", "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", "Benin", "Bermuda", "Bhutan", "Bolivia", "Bosnia & Herzegovina", "Botswana", "Brazil", "British Virgin Islands", "Brunei", "Bulgaria", "Burkina Faso", "Burundi", "Cambodia", "Cameroon", "Canada", "Cape Verde", "Cayman Islands", "Central Arfrican Republic", "Chad", "Chile", "China", "Colombia", "Congo", "Cook Islands", "Costa Rica", "Cote D Ivoire", "Croatia", "Cuba", "Curacao", "Cyprus", "Czech Republic", "Denmark", "Djibouti", "Dominica", "Dominican Republic", "Ecuador", "Egypt", "El Salvador", "Equatorial Guinea", "Eritrea", "Estonia", "Ethiopia", "Falkland Islands", "Faroe Islands", "Fiji", "Finland", "France", "French Polynesia", "French West Indies", "Gabon", "Gambia", "Georgia", "Germany", "Ghana", "Gibraltar", "Greece", "Greenland", "Grenada", "Guam", "Guatemala", "Guernsey", "Guinea", "Guinea Bissau", "Guyana", "Haiti", "Honduras", "Hong Kong", "Hungary", "Iceland", "India", "Indonesia", "Iran", "Iraq", "Ireland", "Isle of Man", "Israel", "Italy", "Jamaica", "Japan", "Jersey", "Jordan", "Kazakhstan", "Kenya", "Kiribati", "Kosovo", "Kuwait", "Kyrgyzstan", "Laos", "Latvia", "Lebanon", "Lesotho", "Liberia", "Libya", "Liechtenstein", "Lithuania", "Luxembourg", "Macau", "Macedonia", "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali", "Malta", "Marshall Islands", "Mauritania", "Mauritius", "Mexico", "Micronesia", "Moldova", "Monaco", "Mongolia", "Montenegro", "Montserrat", "Morocco", "Mozambique", "Myanmar", "Namibia", "Nauro", "Nepal", "Netherlands", "Netherlands Antilles", "New Caledonia", "New Zealand", "Nicaragua", "Niger", "Nigeria", "North Korea", "Norway", "Oman", "Pakistan", "Palau", "Palestine", "Panama", "Papua New Guinea", "Paraguay", "Peru", "Philippines", "Poland", "Portugal", "Puerto Rico", "Qatar", "Reunion", "Romania", "Russia", "Rwanda", "Saint Pierre & Miquelon", "Samoa", "San Marino", "Sao Tome and Principe", "Saudi Arabia", "Senegal", "Serbia", "Seychelles", "Sierra Leone", "Singapore", "Slovakia", "Slovenia", "Solomon Islands", "Somalia", "South Africa", "South Korea", "South Sudan", "Spain", "Sri Lanka", "St Kitts & Nevis", "St Lucia", "St Vincent", "Sudan", "Suriname", "Swaziland", "Sweden", "Switzerland", "Syria", "Taiwan", "Tajikistan", "Tanzania", "Thailand", "Timor L'Este", "Togo", "Tonga", "Trinidad & Tobago", "Tunisia", "Turkey", "Turkmenistan", "Turks & Caicos", "Tuvalu", "Uganda", "Ukraine", "United Arab Emirates", "United Kingdom", "United States of America", "Uruguay", "Uzbekistan", "Vanuatu", "Vatican City", "Venezuela", "Vietnam", "Virgin Islands (US)", "Yemen", "Zambia", "Zimbabwe"];
-
-    /*initiate the autocomplete function on the "myInput" element, and pass along the countries array as possible autocomplete values:*/
-    autocomplete(document.getElementById("myInput"), countries);
-</script> -->
 
 <?php include 'footer_layout.php';  ?>
